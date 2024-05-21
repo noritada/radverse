768fd39e17dcdacabe1d1a5c2ff45f526687a3a0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-05-21 22:53:03 +0900

    Implement coordinate system conversions

--- src/spherical.rs
+++ src/spherical.rs
@@ -4,6 +4,30 @@ pub struct LatLonInDegrees(pub f64, pub f64);
 #[derive(Debug, PartialEq)]
 pub struct Xyz(f64, f64, f64);
 
+impl Xyz {
+    fn unit_normal_vector(&self, other: &Self) -> Self {
+        let vec = self.cross_product(other);
+        let len = vec.norm();
+        Self(vec.0 / len, vec.1 / len, vec.2 / len)
+    }
+
+    fn cross_product(&self, other: &Self) -> Self {
+        Self(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
+    }
+
+    fn dot_product(&self, other: &Self) -> f64 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    }
+
+    fn norm(&self) -> f64 {
+        (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
+    }
+}
+
 impl From<&LatLonInRadians> for Xyz {
     fn from(value: &LatLonInRadians) -> Self {
         let LatLonInRadians(lat, lon) = value;
@@ -102,5 +126,29 @@ mod tests {
         (lat_lon_deg_to_lat_lon_rad_x, 0.0, 0.0, 1.0, 0.0, 0.0,),
         (lat_lon_deg_to_lat_lon_rad_y, 0.0, 90.0, 0.0, 1.0, 0.0,),
         (lat_lon_deg_to_lat_lon_rad_z, 90.0, 0.0, 0.0, 0.0, 1.0,),
+    }
+
+    #[test]
+    fn unit_normal_vector() {
+        let actual = Xyz(1., 1., 0.).unit_normal_vector(&Xyz(0., 1., 1.));
+        let element = (1_f64 / 3.).sqrt();
+        let expected = Xyz(element, -element, element);
+        assert_almost_eq!(actual.0, expected.0, 1.0e-15);
+        assert_almost_eq!(actual.1, expected.1, 1.0e-15);
+        assert_almost_eq!(actual.2, expected.2, 1.0e-15);
+    }
+
+    #[test]
+    fn cross_product() {
+        let actual = Xyz(1., 1., 0.).cross_product(&Xyz(0., 1., 1.));
+        let expected = Xyz(1., -1., 1.);
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn dot_product() {
+        let actual = Xyz(1., 1., 0.).dot_product(&Xyz(0., 1., 1.));
+        let expected = 1.0;
+        assert_eq!(actual, expected)
     }
 }

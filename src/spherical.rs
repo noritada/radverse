0343470780498040f23b89820ca2db71cdaca333
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-07-29 15:49:11 +0900

    Make `assert_almost_eq!` helper macro output informative messages

--- src/spherical.rs
+++ src/spherical.rs
@@ -234,8 +234,16 @@ mod tests {
 
     macro_rules! assert_almost_eq {
         ($a1:expr, $a2:expr, $d:expr) => {
-            if $a1 - $a2 > $d || $a2 - $a1 > $d {
-                panic!();
+            if $a1 - $a2 > $d {
+                panic!(
+                    "assertion a1 - a2 <= delta failed\n a1 - a2: {} - {}\n   delta: {}",
+                    $a1, $a2, $d
+                );
+            } else if $a2 - $a1 > $d {
+                panic!(
+                    "assertion a2 - a1 <= delta failed\n a2 - a1: {} - {}\n   delta: {}",
+                    $a2, $a1, $d
+                );
             }
         };
     }

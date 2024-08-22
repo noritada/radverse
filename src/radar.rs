cedf6fd7f92a5b405c4d20947d97f89f599406b0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-08-23 00:49:33 +0900

    Add a function to do more radar beam path coordinate conversions

--- src/radar.rs
+++ src/radar.rs
@@ -98,9 +98,33 @@ fn calc_altitude_and_distance_on_sphere_inverse(
     RadarObsCellVertical { r_meter, el_deg }
 }
 
+// Formula:
+// ```
+// Z = r_eff + z
+// theta = s / r_eff
+// X = Z * sin(theta)
+// Y = Z * cos(theta) - sr
+// X = r * cos(el)
+// Y = r * sin(el)
+// r = sr * sin(theta) / cos(el + theta)
+// Z = sr * cos(el) / cos(el + theta)
+// ```
+pub fn calc_r_and_z_from_s_and_el(s_meter: f64, el_deg: f64, site: &RadarSite) -> (f64, f64) {
+    let el = el_deg.to_radians();
+    let r_earth = crate::earth::calc_earth_radius(site.lat_deg.to_radians());
+    let r_eff = r_earth * 4_f64 / 3_f64;
+    let sr = r_eff + site.alt_meter;
+    let theta = s_meter / r_eff;
+    let c = sr / (el + theta).cos();
+    let r = c * theta.sin();
+    let z = c * el.cos() - r_eff;
+    (r, z)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test_helpers::*;
 
     #[test]
     fn altitude_and_distance_calculation() {
@@ -116,5 +140,27 @@ mod tests {
         let radar_centered = RadarCenteredPoint::from((&point_original, &site));
         let point_calculated = RadarObsCellVertical::from((&radar_centered, &site));
         assert_eq!(point_calculated, point_original);
+    }
+
+    #[test]
+    fn r_and_z_calculation() {
+        let site = RadarSite {
+            lat_deg: 36.,
+            lon_deg: 140.,
+            alt_meter: 30.,
+        };
+        let (s_meter, el_deg) = (50_000., 10.);
+        let (r_meter, z_meter) = calc_r_and_z_from_s_and_el(s_meter, el_deg, &site);
+
+        let point = RadarObsCellVertical { r_meter, el_deg };
+        let radar_centered = RadarCenteredPoint::from((&point, &site));
+        assert_almost_eq!(radar_centered.dist_meter, s_meter, 1.0e-10);
+
+        let radar_centered = RadarCenteredPoint {
+            alt_meter: z_meter,
+            dist_meter: s_meter,
+        };
+        let point_calculated = RadarObsCellVertical::from((&radar_centered, &site));
+        assert_almost_eq!(point_calculated.el_deg, el_deg, 1.0e-10);
     }
 }

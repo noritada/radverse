2db2c21d2adc1e722d36351bed2e63d6effc458a
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2024-09-18 23:13:51 +0900

    Implement the transformation to the local coordinate

--- src/spherical.rs
+++ src/spherical.rs
@@ -142,6 +142,35 @@ impl LocalXyzTransformation {
         let y = z.cross_product(&x);
         Self([x, y, z])
     }
+
+    pub(crate) fn transform(&self, xyz: &Xyz) -> Xyz {
+        let Self([ax, ay, az]) = self;
+        Xyz(
+            ax.dot_product(&xyz),
+            ay.dot_product(&xyz),
+            az.dot_product(&xyz),
+        )
+    }
+
+    pub(crate) fn transform_inverse(&self, xyz: &Xyz) -> Xyz {
+        let Self([ax, ay, az]) = self;
+        let inv_c = 1.
+            / (ax.0 * ay.1 * az.2 + ax.1 * ay.2 * az.0 + ax.2 * ay.0 * az.1
+                - ax.2 * ay.1 * az.0
+                - ax.1 * ay.0 * az.2
+                - ax.0 * ay.2 * az.1);
+        let tmp_x = ay.cross_product(&az);
+        let tmp_y = az.cross_product(&ax);
+        let tmp_z = ax.cross_product(&ay);
+        let inv_x = Xyz(tmp_x.0, tmp_y.0, tmp_z.0);
+        let inv_y = Xyz(tmp_x.1, tmp_y.1, tmp_z.1);
+        let inv_z = Xyz(tmp_x.2, tmp_y.2, tmp_z.2);
+        Xyz(
+            inv_x.dot_product(&xyz) * inv_c,
+            inv_y.dot_product(&xyz) * inv_c,
+            inv_z.dot_product(&xyz) * inv_c,
+        )
+    }
 }
 
 pub struct VerticalCrossSection {
@@ -659,6 +688,125 @@ mod tests {
         (
             local_xyz_transformation_with_z_axis_centered,
             LatLonInDegrees(90., 0.), [Xyz(0., 1., 0.), Xyz(-1., 0., 0.), Xyz(0., 0., 1.)]
+        ),
+    }
+
+    macro_rules! test_local_xyz_transformation_forward_operations {
+        ($((
+            $name:ident,
+            $center:expr,
+            $input:expr,
+            $expected:expr
+        ),)*) => ($(
+            #[test]
+            fn $name() {
+                let center = $center;
+                let tx = LocalXyzTransformation::from(&LatLonInRadians::from(&center));
+                let input = $input;
+                let actual = tx.transform(&input);
+                let expected = $expected;
+                assert_almost_eq!(actual.0, expected.0, 1.0e-15);
+                assert_almost_eq!(actual.1, expected.1, 1.0e-15);
+                assert_almost_eq!(actual.2, expected.2, 1.0e-15);
+            }
+        )*);
+    }
+
+    test_local_xyz_transformation_forward_operations! {
+        (
+            local_xyz_forward_transformation_of_x_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(1., 0., 0.), Xyz(0., 0., 1.)
+        ),
+        (
+            local_xyz_forward_transformation_of_y_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(0., 1., 0.), Xyz(1., 0., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_z_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(0., 0., 1.), Xyz(0., 1., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_x_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(1., 0., 0.), Xyz(-1., 0., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_y_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(0., 1., 0.), Xyz(0., 0., 1.)
+        ),
+        (
+            local_xyz_forward_transformation_of_z_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(0., 0., 1.), Xyz(0., 1., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_x_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(1., 0., 0.), Xyz(0., -1., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_y_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(0., 1., 0.), Xyz(1., 0., 0.)
+        ),
+        (
+            local_xyz_forward_transformation_of_z_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(0., 0., 1.), Xyz(0., 0., 1.)
+        ),
+    }
+
+    macro_rules! test_local_xyz_transformation_inverse_operations {
+        ($((
+            $name:ident,
+            $center:expr,
+            $input:expr
+        ),)*) => ($(
+            #[test]
+            fn $name() {
+                let center = $center;
+                let tx = LocalXyzTransformation::from(&LatLonInRadians::from(&center));
+                let input = $input;
+                let actual = tx.transform_inverse(&tx.transform(&input));
+                let expected = input;
+                assert_almost_eq!(actual.0, expected.0, 1.0e-15);
+                assert_almost_eq!(actual.1, expected.1, 1.0e-15);
+                assert_almost_eq!(actual.2, expected.2, 1.0e-15);
+            }
+        )*);
+    }
+
+    test_local_xyz_transformation_inverse_operations! {
+        (
+            local_xyz_inverse_transformation_of_x_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(1., 0., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_y_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(0., 1., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_z_axis_to_center_on_x_axis,
+            LatLonInDegrees(0., 0.), Xyz(0., 0., 1.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_x_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(1., 0., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_y_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(0., 1., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_z_axis_to_center_on_y_axis,
+            LatLonInDegrees(0., 90.), Xyz(0., 0., 1.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_x_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(1., 0., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_y_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(0., 1., 0.)
+        ),
+        (
+            local_xyz_inverse_transformation_of_z_axis_to_center_on_z_axis,
+            LatLonInDegrees(90., 0.), Xyz(0., 0., 1.)
         ),
     }
 
